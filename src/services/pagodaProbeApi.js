c49e9d4b25a3b6c1b59812a677947c569722b5f1
Gabe Arnold <gabearnold@Gabes-MacBook-Pro.local>
2024-06-23 10:57:52 -0400

    update Pagoda Probe API endpoint env variable

--- src/services/pagodaProbeApi.js
+++ src/services/pagodaProbeApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // https://redux-toolkit.js.org/rtk-query/overview
 export const pagodaProbeApi = createApi({
     reducerPath: "pagodaProbeApi",
-    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.PAGODA_PROBE_API_ENDPOINT }),
+    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_PAGODA_PROBE_API_ENDPOINT }),
     endpoints: (builder) => ({
         getDocketCaseByNumber: builder.query({
             query: (number) => `docketcase/getcasebydocketnumber?docketNumber=${number}`
